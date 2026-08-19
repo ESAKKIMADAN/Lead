578bba6a977393d2f8fcbc6559694fccb479d062
ESAKKIMADAN <esakkimadan10@gmail.com>
2026-08-19 08:48:57 +0530

    fix: resolve syntax error in AccountView

--- src/components/AccountView.tsx
+++ src/components/AccountView.tsx
@@ -95,10 +95,6 @@ export default function AccountView({ onBack }: AccountViewProps) {
     }
   };
 
-  const urlBase64ToUint8Array = (base64String: string) => {
-    const padding = '='.repeat((4 - base64String.length % 4) % 4);
-
-
   const handleReset = async () => {
     setSaving(true);
     await resetAllData();
